11def0c2b97b1dcd03c6e44ec15597bcf92e6901
eriika19 <enciso.iq@gmail.com>
2019-12-03 17:10:45 -0600

    se dan toques finales

--- pages/question.js
+++ pages/question.js
@@ -3,6 +3,8 @@ import Head from "next/head";
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import Fade from "react-reveal/Fade";
+import "hover.css/css/hover-min.css";
+
 
 //import Oops from "../components/Oops";
 import Layout from "../components/Layout";
@@ -21,68 +23,148 @@ class QuestionPage extends Component {
 
     return question ? (
       <Layout>
-        <Fade top>        
-        <section id="interview" className="container">
-          <Head>
-            <title>Runa | Interview</title>
-          </Head>
-          <section className="hero">
-            <User user={user} thumbnail={thumbnail} role={role} />
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title"> Pregunta {id}</h1>
-                <h2 className="subtitle">{question}</h2>
+        <Fade top>
+          <section id="interview" className="container">
+            <Head>
+              <title>Runa | Interview</title>
+            </Head>
+            <section className="hero">
+              <User user={user} thumbnail={thumbnail} role={role} />
+              <div className="hero-body">
+                <div className="container">
+                  <h1 className="title"> Pregunta {id}</h1>
+                  <h2 className="subtitle">{question}</h2>
+                </div>
               </div>
-            </div>
+            </section>
+            <section className="columns level">
+              <div className="column level-item">
+                <div className="hvr-grow">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/1.png" alt="point" />
+                  </figure>
+                  <p className="">Puedo mejorar</p>
+                </div>
+              </div>
+              <div className="column level-item">
+                <div className="hvr-grow">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/2.png" alt="point" />
+                  </figure>
+                  <p className="">Soy bueno</p>
+                </div>
+              </div>
+              <div className="column level-item">
+                <div className="hvr-grow">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/3.png" alt="point" />
+                  </figure>
+                  <p className="">Esta es mi habilidad destacada</p>
+                </div>
+              </div>
+            </section>
+            <QuestionBtn text="Siguiente" id={nextID} />
           </section>
-          <section className="columns">
-            <div className="column">Puede mejorar</div>
-            <div className="column">Me parece bueno</div>
-            <div className="column">Esta es mi habilidad destacada</div>
-          </section>
-          <QuestionBtn text="Siguiente" id={nextID} />
-          </section>
-          </Fade>
+        </Fade>
+        <style jsx global>
+          {`
+            .columns {
+              width: 100%;
+              min-height: 22vh;
+            }
+            .column {
+              align-items: center;
+              justify-content: center;
+              text-align: center;
+            }
+          `}
+        </style>
       </Layout>
     ) : (
-      
       <Layout>
         <Fade left>
-          
-        <section id="interview" className="container">
-          <Head>
-            <title>Runa | Interview</title>
-          </Head>
-          <section className="hero">
-            <User user="" thumbnail="/assets/Lizzie.jpg" />
-            <div className="hero-body">
-              <div className="container">
-                <h1 className="title"> ¡Genial!</h1>
-                <h2 className="subtitle">
-                  Esto es lo que has mejorado este año
-                </h2>
+          <section id="interview" className="container">
+            <Head>
+              <title>Runa | Interview</title>
+            </Head>
+            <section className="hero">
+              <User user="" thumbnail="/assets/Lizzie.jpg" />
+              <div className="hero-body">
+                <div className="container">
+                  <h1 className="title"> ¡Genial!</h1>
+                  <h2 className="subtitle">
+                    Esto es lo que has mejorado este año
+                  </h2>
+                </div>
               </div>
+            </section>
+            <section className="columns level">
+              <div className="column level-item">
+                <div className="">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/circle-hi.png" alt="point" />
+                  </figure>
+                  <p className="">Actuamos como owners</p>
+                </div>
+              </div>
+              <div className="column level-item">
+                <div className="">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/circle-hi.png" alt="point" />
+                  </figure>
+                  <p className="">Jugamos en equipo</p>
+                </div>
+              </div>
+              <div className="column level-item">
+                <div className="">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/circle-hi.png" alt="point" />
+                  </figure>
+                  <p className="">Aspiramos a la excelencia</p>
+                </div>
+              </div>
+            </section>
+            <section className="columns level">
+              <div className="column level-item">
+                <div className="">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/circle-vi.png" alt="point" />
+                  </figure>
+                  <p className="">Experimentamos constantemente</p>
+                </div>
+              </div>
+              <div className="column level-item">
+                <div className="">
+                  <figure className=" icon is-large">
+                    <img className="" src="/assets/circle-vi.png" alt="point" />
+                  </figure>
+                  <p className="">Tenemos libertad con responsabilidad</p>
+                </div>
+              </div>
+            </section>
+            <div className="field has-text-centered">
+              <Link href="/suggest">
+                <button className="button is-succes is-outlined is-rounded">
+                  Continuar
+                </button>
+              </Link>
             </div>
           </section>
-          <section className="columns">
-            <div className="column">Actuamos como owners</div>
-            <div className="column">Aspiramos a la experiencia</div>
-            <div className="column">Jugamos en equipo</div>
-          </section>
-          <section className="columns">
-            <div className="column">Experimentamos constantemente</div>
-            <div className="column">Tenemos libertad con responsabilidad</div>
-          </section>
-          <div className="field has-text-centered">
-            <Link href="/suggest">
-              <button className="button is-succes is-outlined is-rounded">
-                Continuar
-              </button>
-            </Link>
-          </div>
-        </section>
-          </Fade>
-          </Layout>
+        </Fade>
+        <style jsx global>
+          {`
+            .columns {
+              width: 100%;
+              min-height: 22vh;
+            }
+            .column {
+              align-items: center;
+              justify-content: center;
+              text-align: center;
+            }
+          `}
+        </style>
+      </Layout>
     );
   }
 }
@@ -90,7 +172,7 @@ class QuestionPage extends Component {
 QuestionPage.getInitialProps = async ({ query: { id } }) => {
   const index = parseInt(id, 10) - 1;
 
-  const res = await fetch("https://sheetsu.com/apis/v1.0su/fdf10260eb2b");
+  const res = await fetch("https://sheetsu.com/apis/v1.0su/a25c2c2fd334");
   const data = await res.json();
 
   if (data[index] !== undefined) {
